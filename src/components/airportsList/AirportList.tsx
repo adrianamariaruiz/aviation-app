fc6d9df90be32eeb9c75398c89c496051cb89117
Adriana <adriannaruiz24@gmail.com>
2025-03-30 18:25:14 -0500

    feature data mock and table with info

--- src/components/airportsList/AirportList.tsx
+++ src/components/airportsList/AirportList.tsx
@@ -1,41 +1,44 @@
 import { IAirport } from "@/interfaces/airports.interface";
 import { ICountry } from "@/interfaces/countries.interface";
 
-interface Props{
+interface Props {
   airports: IAirport[];
   countries: ICountry[];
 }
 
-const AirportsList = ({airports, countries}:Props) => {
+const AirportsList = ({ airports, countries }: Props) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {
-        
-        airports.map((airport) => {
-          
-          const airportCountry = countries.find(country => country.country_iso2 === airport.country_iso2);
-          console.log(airportCountry)
+    <div className="flex justify-center w-full gap-4">
+      <div className="relative overflow-x-auto w-full shadow-md sm:rounded-lg xl:w-3/5">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-md text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">Aeropuerto</th>
+              <th scope="col" className="px-6 py-3">Pais</th>
+              <th scope="col" className="px-6 py-3">Ciudad</th>
+              <th scope="col" className="px-6 py-3">Código</th>
+            </tr>
+          </thead>
+          <tbody >
+            {
+              airports.map((airport) => {
 
-          return(
-            <div key={airport.airport_id} className="flex flex-col border-2 rounded-lg shadow-md p-4 gap-2">
-              <h2 className="text-2xl font-semibold">{airport.airport_name}</h2>
-              {
-                airportCountry && 
-                  <div key={airportCountry.country_id}>
-                    <div>
-                      <p className="text-l">{airportCountry.capital}, {airportCountry.country_name}</p>
-                      <p className="font-extrabold w-fit text-3xl bg-linear-90 from-[#006AFF] to-[#00F9FF] text-transparent bg-clip-text">{airportCountry.currency_code}</p>
-                    </div>
-                  </div>
-                  
-              }
-            </div>
-          )
-
-        })
-
-      }
-
+                const airportCountry = countries.find(country => country.country_iso2 === airport.country_iso2);
+                const airpotTimezone = airport.timezone.split("/")[1].replace(/_/g, " ");
+                
+                return (
+                  <tr key={airport.airport_id} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{airport.airport_name}</th>
+                    <td className="px-6 py-3">{airpotTimezone}</td>
+                    <td className="px-6 py-3">{airport.country_name}</td>
+                    <td className="px-6 py-3 font-extrabold w-fit text-xl bg-linear-90 from-[#006AFF] to-[#00F9FF] text-transparent bg-clip-text">{airportCountry?.currency_code}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
